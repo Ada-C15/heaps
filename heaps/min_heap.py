1a38148bcab2a386be1e11751ff5bce51186fbb8
Ari <ari.go.org.7@gmail.com>
2021-10-21 20:05:21 -0700

    heap up - strfy fixed --  add method is  wip

--- heaps/min_heap.py
+++ heaps/min_heap.py
@@ -1,28 +1,23 @@
+
 class HeapNode:
 
-    def initialize(self, key, value):
+
+    def __init__(self, key, value):
         self.key = key
         self.value = value
+        # self.left = none
+        # self.right = none 
 
 class MinHeap:
 
     def __init__(self):
         self.store = []
-        if not self.store:
-            self.root = None
-            
-        else:
-            self.root = self.store[0]
+        # self.root = None
 
-        # children nodes?
-        # for i in range(len(self.store)):
-        #     left_child = i * 2 + 1
-        #     right_child = i * 2 + 2
-        #     left_parent = i % 2 - 1
+        # left_child = parent * 2 + 1
         
-        #     right_child = i * 2 + 2
+        # right_child = parent * 2 + 2
     
-
     # def dict(self):
         # return {'key': self.key, 'value': self.value}
 
@@ -32,23 +27,21 @@ class MinHeap:
             Time Complexity: ?
             Space Complexity: ?
         """
+        # min heap
+        # [5, 10, 21, 11, 14, 25, 31]
+        # [0,  1, 2,  3,  4,  5,  6]
+
+        # append a node
         if value == None:
             value = key
         
-        if self.empty:
-            self.store = HeapNode(key, value)
+        # adding it to the end of the heap if heap not empty 
+        if self.empty():
+            self.store.append(HeapNode(key,value))  
             return
 
-        # adding it to the end of the heap if heap not empty
         self.store.append(HeapNode(key, value))
-
-        # # Compare the new node with it's parent
-        parent_node = self.root
-        # If they are out of order swap and heap-up
-        # using the parent's index number.
-        # Implementation not shown purposefully.
-        # self.heap_up(self.store.length - 1)
-
+        self.heap_up((len(self.store))- 1)
 
     def remove(self):
         """ This method removes and returns an element from the heap
@@ -58,14 +51,12 @@ class MinHeap:
         """
         pass
 
-
-    
     def __str__(self):
         """ This method lets you print the heap, when you're testing your app.
         """
         if len(self.store) == 0:
             return "[]"
-        return f"[{', '.join([str(element) for element in self.store])}]"
+        return f"[{', '.join([str(element.value) for element in self.store])}]"
 
 
     def empty(self):
@@ -83,8 +74,18 @@ class MinHeap:
             Time complexity: ?
             Space complexity: ?
         """
-        # # Compare the new node with it's parent
-        if self.
+         # left_child = index * 2 + 1
+        # right_child = index * 2 + 2
+        parent_index = (index-1) // 2
+        parent = self.store[parent_index] 
+        new_node = self.store[index]
+ 
+        while new_node.value < parent.value:
+            self.swap(parent_index, index)
+            new_node = self.store[parent_index] # updating where parent was 
+            parent_index = (parent_index-1)//2 #  i = 0 # recalculation  new index  - figure out new parent
+            parent = self.store[parent_index] # value of parent
+
         # If they are out of order swap and heap-up
         # using the parent's index number.
         # Implementation not shown purposefully.
