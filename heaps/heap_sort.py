0a34dee39b2a9ceeab3c558151854fdf86bd8795
mairandomness <mairakodama@gmail.com>
2021-10-28 17:14:48 -0700

    Add solutions

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,17 @@
-
+from .min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(n log n)
+        Space Complexity: O(n)
     """
-    pass
+    heap = MinHeap()
+    for elem in list:
+        heap.add(elem)
+    
+    sorted_list = []
+    while heap.store:
+        elem = heap.remove()
+        sorted_list.append(elem)
+    
+    return sorted_list
