23621eaf86739e198917bc0bf9fac2a2a50bf00b
Jittania Smith <c-jismith@zulily.com>
2021-11-14 22:04:44 -0800

    passing all tests

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,18 @@
-
+from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
         Time Complexity:  ?
         Space Complexity: ?
     """
-    pass
+    heap_o_frogges = MinHeap()
+
+    for num in list:
+        heap_o_frogges.add(num)
+
+    i = 0
+    while not heap_o_frogges.empty():
+        list[i] = heap_o_frogges.remove()
+        i += 1
+    
+    return list 
--- heaps/min_heap.py
+++ heaps/min_heap.py
@@ -1,5 +1,5 @@
 class HeapNode:
-  
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -15,27 +15,45 @@ class HeapNode:
 class MinHeap:
 
     def __init__(self):
-        self.store = []
+        self.store = [] # Intializing our empty heap
 
 
     def add(self, key, value = None):
         """ This method adds a HeapNode instance to the heap
             If value == None the new node's value should be set to key
-            Time Complexity: ?
-            Space Complexity: ?
+            Time Complexity: O(log n)
+            Worst case would be if curr node is smaller than every other parent node such that it becomes the new root, causing a full traversel of the tree's height, which is log(n)
+            Space Complexity: 0(1)
         """
-        pass
+        if value == None:
+            value = key
+
+        # Add curr node to the bottom of the tree
+        curr_node = HeapNode(key, value)
+        self.store.append(curr_node)
+
+        # Keep swapping until a smaller parent node is found
+        self.heap_up(len(self.store) - 1)
+
 
     def remove(self):
-        """ This method removes and returns an element from the heap
+        """ This method removes and returns a smollest_frogge from the heap
             maintaining the heap structure
-            Time Complexity: ?
-            Space Complexity: ?
+            Time Complexity: O(log n)
+            Space Complexity: 0(1)
         """
-        pass
+        if len(self.store) == 0:
+            return
+
+        self.swap(0, len(self.store)-1)
+
+        smollest_frogge = self.store.pop()
+
+        self.heap_down(0)
+
+        return smollest_frogge.value
 
 
-    
     def __str__(self):
         """ This method lets you print the heap, when you're testing your app.
         """
@@ -46,38 +64,60 @@ class MinHeap:
 
     def empty(self):
         """ This method returns true if the heap is empty
-            Time complexity: ?
-            Space complexity: ?
+            Time complexity: O(1)
+            Space complexity: O(1)
         """
-        pass
+        return len(self.store) == 0
 
 
     def heap_up(self, index):
         """ This helper method takes an index and
             moves the corresponding element up the heap, if 
-            it is less than it's parent node until the Heap
+            it is less than its parent node until the Heap
             property is reestablished.
-            
-            This could be **very** helpful for the add method.
-            Time complexity: ?
-            Space complexity: ?
+
+            Time complexity: O(log n)
+            Space complexity: 0(1)
         """
-        pass
+        if index == 0:
+            return index
+
+        parent_node = (index - 1) // 2
+
+        if self.store[parent_node].key > self.store[index].key:
+            self.swap(parent_node, index)
+            self.heap_up(parent_node)
+
+
 
     def heap_down(self, index):
         """ This helper method takes an index and 
-            moves the corresponding element down the heap if it's 
-            larger than either of its children and continues until
+            moves the corresponding frogge down the heap if it's 
+            larger than either of its children frogges and continues until
             the heap property is reestablished.
         """
-        pass
+        frogge_heap = self.store
+        l_child_frogge = (2*index) + 1
+        r_child_frogge = (2*index) + 2
 
+        if l_child_frogge < len(frogge_heap):
+            if r_child_frogge < len(frogge_heap):
+                if frogge_heap[l_child_frogge].key < frogge_heap[r_child_frogge].key:
+                    smoller_frogge = l_child_frogge
+                else:
+                    smoller_frogge = r_child_frogge
+            else:
+                smoller_frogge = l_child_frogge
+
+            if frogge_heap[index].key > frogge_heap[smoller_frogge].key:
+                self.swap(index, smoller_frogge)
+                self.heap_down(smoller_frogge)
     
     def swap(self, index_1, index_2):
         """ Swaps two elements in self.store
             at index_1 and index_2
             used for heap_up & heap_down
         """
-        temp = self.store[index_1]
+        index_1_store = self.store[index_1]
         self.store[index_1] = self.store[index_2]
-        self.store[index_2] = temp
+        self.store[index_2] = index_1_store
