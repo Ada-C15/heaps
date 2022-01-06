88af095a036bdf30e732f792c6d2019a2630e894
Alli-Oops <AllisonLeonardSeattle@gmail.com>
2022-01-05 19:40:53 -0800

    passing tests

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,16 @@
-
+from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
         Time Complexity:  O(log n)
         Space Complexity: ?
     """
-    pass
+    heap = MinHeap()
+    for num in list:
+        heap.add(num)
+    
+    heap_list = []
+    while heap.store:
+        num = heap.remove()
+        heap_list.append(num)
+    return heap_list
--- heaps/min_heap.py
+++ heaps/min_heap.py
@@ -5,56 +5,46 @@ class HeapNode:
 
     def __str__(self):
         return str(self.value)
-
-# We begin our implementation of a binary heap with the constructor. 
-# Since the entire binary heap can be represented by a single list, 
-# we could make it so that
-# all the constructor will do is initialize the list and an attribute currentSize to track the current size of the heap.
-# class BinHeap:
-#     def initialize(self):
-#         self.heapList = [0] # the zero is there so that simple integer division 
-#         self.currentSize = 0
+        
 class MinHeap:
     def __init__(self):
         self.store = [] # this is the list where we store stuff
 
-    def parent():
-    
-    def find_left_child():
-
-    def find_right_child():
-        
-    
-
     def add(self, key, value = None):
-        """ This method adds a HeapNode instance to the heap
-            If value == None the new node's value should be set to key
-            Time Complexity: O(logn)
-            Space Complexity: O(n)
+        """ 
+        This method adds a HeapNode instance to the heap
+        If value == None the new node's value should be set to key
+        Time Complexity: O(logn)
+        Space Complexity: O(n)
         """
-        self.store.append(HeapNode.new(key,value))
-    # we will append a new value to the end of the 
-    # Compare the new node with it's parent
-    # If they are out of order swap and heap-up
-    # using the parent's index number.
+        if not value:
+            value = key
+        self.store.append(HeapNode(key, value))
+        # we will append a new value to the end of the heap
+        # Compare the new node with it's parent
+        # If they are out of order swap and heap-up
+        # using the parent's index number.
         self.heap_up(len(self.store) - 1)
 
     def remove(self):
-        """ This method removes and returns an element from the heap
-            maintaining the heap structure
-            Time Complexity: ?
-            Space Complexity: ?
+        """ 
+        This method removes and returns an element from the heap
+        maintaining the heap structure
+        Time Complexity: ?
+        Space Complexity: ?
         """
+        if self.empty():
+            return None
 
-        if self.store.empty?
-        return nil
+        self.swap(0, len(self.store) - 1)
+        remove_val = self.store.pop()
+        self.heap_down(0)
 
-        self.swap(0, self.store.last - 1)
-        result = self.store.pop()
-
+        return remove_val.value
     
     def __str__(self):
-        """ This method lets you print the heap, when you're testing your app.
+        """ 
+        This method lets you print the heap, when you're testing your app.
         """
         if len(self.store) == 0:
             return "[]"
@@ -62,73 +52,57 @@ class MinHeap:
 
 
     def empty(self):
-        """ This method returns true if the heap is empty
-            Time complexity: ?
-            Space complexity: ?
+        """ 
+        This method returns true if the heap is empty
+        Time complexity: O(1)
+        Space complexity: O(1)
         """
-        pass
+        return len(self.store) == 0
 
 
     def heap_up(self, index):
-        """ This helper method takes an index and
-            moves the corresponding element up the heap, if 
-            it is less than it's parent node until the Heap
-            property is reestablished.
-            
-            This could be **very** helpful for the add method.
-            Time complexity: ?
-            Space complexity: ?
         """
-        while self.store[parent].key > self.store[index].key:
-            if index == 0:
-                break
-            self.swap(index, parent)
-            index = parent
-            parent = self.find_parent(index)
+        This helper method takes an index and
+        moves the corresponding element up the heap, if 
+        it is less than it's parent node until the Heap
+        property is reestablished.
+        This could be **very** helpful for the add method.
+        Time complexity: O()
+        Space complexity: 0()
+        """
+        parent_index = (index-1)//2
+        while index>0 and self.store[index].key < self.store[parent_index].key:
+            self.swap(index,parent_index)
+            index = parent_index
+            parent_index = (index-1)//2
 
     def heap_down(self, index):
-        """ This helper method takes an index and 
-            moves the corresponding element down the heap if it's 
-            larger than either of its children and continues until
-            the heap property is reestablished.
-            than it's parent node.
+        """ 
+        This helper method takes an index and 
+        moves the corresponding element down the heap if it's 
+        larger than either of its children and continues until
+        the heap property is reestablished.
+        than it's parent node.
         """
-        smallest_child = self.find_smallest_child(index)
-        if smallest_child and index < len(self.store):
-            if self.store[index].key > self.store[smallest_child].key:
-            
-        # start heap_down with the root (index 0)
-        lc = self.find_left_child(index)
-        rc = self.find_right_child(index)
-        while not self.store[index].key <= self.store[lc].key or not self.store[index].key <= self.store[rc].key:
-            if self.store[index].key > self.store[lc].key and self.store[lc].key
-                smallest_child = lc
-            else:
-                smallest_child = rc
-            self.swap(smallest_child, index)
-            index = smallest_child
-            lc = self.find_left_child(index)
-            rc = self.find_right_child(index)
-            if lc > len(self.store)-1 or rc > len(self.store)-1:
-                break
-            return
 
-        #self.heap_down(0) unless self.store.empty?
-        #return result
+        childOne = index*2+1
+        childTwo = childOne+1
 
-        while (i * 2) <= self.currentSize:
-        mc = self.minChild(i)
-        if self.heapList[i] > self.heapList[mc]:
-            tmp = self.heapList[i]
-            self.heapList[i] = self.heapList[mc]
-            self.heapList[mc] = tmp
-        i = mc
+        if len(self.store)>childTwo and self.store[childOne].key>self.store[childTwo].key:
+            childOne=childTwo
 
+        if len(self.store)<=childOne:
+            return None
+
+        if self.store[childOne].key<self.store[index].key:
+            self.swap(index,childOne)
+            self.heap_down(childOne)
 
     def swap(self, index_1, index_2):
-        """ Swaps two elements in self.store
-            at index_1 and index_2
-            used for heap_up & heap_down
+        """
+        Swaps two elements in self.store
+        at index_1 and index_2
+        used for heap_up & heap_down
         """
         temp = self.store[index_1]
         self.store[index_1] = self.store[index_2]
