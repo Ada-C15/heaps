a73d0ed7100ee50dc01d923a85d7be063feba1ef
Araceli <aracelimaldonado4@yahoo.com>
2022-01-24 19:09:26 -0800

    all tests passing

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,42 @@
+from heaps.min_heap import MinHeap
 
-
-def heap_sort(list):
+def heap_sort(arr):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O (log n)
+        Space Complexity: O(1)
     """
-    pass
+    n = len(arr)
+  
+    # Build a maxheap.
+    # Since last parent will be at ((n//2)-1) we can start at that location.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+  
+    # One by one extract elements
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]   # swap
+        heapify(arr, i, 0)
+
+    return arr 
+
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1     # left = 2*i + 1
+    right = 2 * i + 2     # right = 2*i + 2
+  
+    # See if left child of root exists and is
+    # greater than root
+    if left < n and arr[i] < arr[left]:
+        largest = left
+  
+    # See if right child of root exists and is
+    # greater than root
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+  
+    # Change root, if needed
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]  # swap
+  
+        # Heapify the root.
+        heapify(arr, n, largest)
