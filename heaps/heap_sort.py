f6d5e93e375ba6df0e8169c125815944dc4a4a5e
mana <manushka@pm.me>
2022-01-26 12:51:19 -0800

    updates Big O

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -3,8 +3,8 @@ from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(log(n))
+        Space Complexity: O(n)
     """
     heap = MinHeap()
     return_list = []
