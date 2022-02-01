8c53596291b3b9b07b8650f04e3dad5be35e5d07
Aida <aida.rysbek@gmail.com>
2022-02-01 22:20:50 +0300

    Add methods, passing tests

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -1,8 +1,19 @@
-
+from heaps.min_heap import MinHeap
 
 def heap_sort(list):
     """ This method uses a heap to sort an array.
-        Time Complexity:  ?
-        Space Complexity: ?
+        Time Complexity:  O(n)
+        Space Complexity: O(n)
     """
-    pass
+    heaps_dont_lie = MinHeap()
+
+    for num in list:
+        heaps_dont_lie.add(num)
+
+    i = 0
+    while not heaps_dont_lie.empty():
+        list[i] = heaps_dont_lie.remove()
+        i += 1
+
+    return list
+    
